ef5efc00363ace44e8a030fceac492ec16db970d
Kyle Aron <aron.kyle@gmail.com>
2023-05-15 01:07:51 -0400

    Remove an unused test theme. Rename some old themes.

--- src/theme/Theme.ts
+++ src/theme/Theme.ts
@@ -32,21 +32,59 @@ declare module '@mui/material/styles' {
     deleteButton?: string;
     deleteButtonHover?: string;
   }
-
 }
-  
-
-export const testTheme = createTheme({
-  body: '#204bda',
-});
 
 export const lightTheme = createTheme({
+palette: {
+    info: {
+    main: '#4b4b4b',
+    }
+},
+name: LIGHT_MODE,
+body: '#E2E2E2',
+headerBody: '#cfcfcf',
+headerBodyHover: '#b1b1b1',
+headerBodyAlternate: '#c4c4c4',
+transparent: '#ffffff00',
+text: '#363537',
+textLighter: '#636363',
+toggleBorder: '#FFF',
+headerButtonBackground: '#6d6d6d',
+deleteButton: '#e40000',
+deleteButtonHover: '#ac0000',
+});
+    
+export const darkTheme = createTheme({
+palette: {
+    info: {
+    main: '#dddddd',
+    }
+},
+name: DARK_MODE,
+body: '#2b2a2c',
+headerBody: '#4d4b4e',
+headerBodyHover: '#535155',
+headerBodyAlternate: '#5a5a5a',
+transparent: '#ffffff00',
+text: '#FAFAFA',
+textLighter: '#cacaca',
+toggleBorder: '#6B8096',
+headerButtonBackground: '#808080',
+deleteButton: '#e40000',
+deleteButtonHover: '#ac0000',
+});
+
+
+////////////////////////////////
+///////  OLD THEMES BELOW
+
+export const lightThemeOLD = createTheme({
   palette: {
     info: {
       main: '#4b4b4b',
     }
   },
-  name: LIGHT_MODE,
+  name: "LIGHT_MODE_OLD",
   body: '#E2E2E2',
   headerBody: '#cfcfcf',
   headerBodyHover: '#b1b1b1',
@@ -60,13 +98,13 @@ export const lightTheme = createTheme({
   deleteButtonHover: '#ac0000',
 });
   
-export const darkTheme = createTheme({
+export const darkThemeOLD = createTheme({
   palette: {
     info: {
       main: '#dddddd',
     }
   },
-  name: DARK_MODE,
+  name: "DARK_MODE_OLD",
   body: '#2b2a2c',
   headerBody: '#4d4b4e',
   headerBodyHover: '#535155',
