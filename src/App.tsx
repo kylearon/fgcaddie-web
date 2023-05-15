17d3a159d3b8268c3e462e5f93b9979f99bc1bfb
Kyle Aron <aron.kyle@gmail.com>
2023-05-15 01:10:42 -0400

    Remove unused code from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
-import { Routes, Route, useLocation, useNavigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
-import { testTheme, darkTheme, lightTheme } from './theme/Theme';
+import { darkTheme, lightTheme } from './theme/Theme';
 import { ThemeProvider } from '@mui/material';
 import { LIGHT_MODE, DARK_MODE } from './utils/constants';
 import HomePage from './views/HomePage/HomePage';
@@ -25,11 +24,6 @@ function App() {
             return darkTheme;
         }
     });
-
-    const navigate = useNavigate();
-
-    const location = useLocation();
-
 
     return (
         <>
